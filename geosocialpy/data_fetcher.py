124fd02eb080d2a1e93482b78ee8b9aa36d9bd2a
Jayesh Suryavanshi <jayeshsuryavanshi808@gmail.com>
2023-06-14 21:32:18 -0400

    New changes in setup file

--- geosocialpy/data_fetcher.py
+++ geosocialpy/data_fetcher.py
@@ -1,28 +1,22 @@
-import json
 import tweepy
+import json
 
 class TwitterDataFetcher:
     def __init__(self, api_key, api_key_secret, access_token, access_token_secret):
         auth = tweepy.OAuthHandler(api_key, api_key_secret)
         auth.set_access_token(access_token, access_token_secret)
-        self.api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+        self.api = tweepy.API(auth, wait_on_rate_limit=True)
 
     def fetch_tweets(self, geocode, count=100):
         try:
-            tweets = self.api.search(q="*", geocode=geocode, count=count)
-            return tweets
-        except tweepy.TweepError as e:
-            print(f"An error occurred: {e}")
+            return tweepy.Cursor(self.api.search_tweets, q="", geocode=geocode, count=count).items()
+        except Exception as e:
+            print(f"Error: {str(e)}")
             return None
 
-    def save_tweets_to_file(self, tweets, filename):
-        with open(filename, 'w') as f:
+    def save_tweets_to_file(self, tweets, file_name):
+        with open(file_name, 'w') as f:
             for tweet in tweets:
                 json.dump(tweet._json, f)
                 f.write('\n')
-
-
-fetcher = TwitterDataFetcher(api_key, api_key_secret, access_token, access_token_secret)
-tweets = fetcher.fetch_tweets("37.7749,-122.4194,10mi", count=100)
-if tweets is not None:
-    fetcher.save_tweets_to_file(tweets, "tweets.json")
--- setup.py
+++ setup.py
@@ -1,7 +1,29 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="geosocialpy",
+    name="GeoSocialPy",
     version="0.1",
     packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "tweepy",
+        "geopy"
+    ],
+    author="Jayesh Kishor Suryavanshi",
+    author_email="jayeshki@buffalo.edu",
+    description="A Python library to fetch and analyze geospatial data from social media",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/JayeshSuryavanshi/GeoSocialPy",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
 )
